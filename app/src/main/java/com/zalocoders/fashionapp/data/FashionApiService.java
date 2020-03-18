ecb1b2ad771ae2bd8ca33de5fe3f1fdd84002c9a
Andrew Ananda <andrewtiema@gmail.com>
2020-03-19 01:24:37 +0300

    fix/data

--- app/src/main/java/com/zalocoders/fashionapp/data/FashionApiService.java
+++ app/src/main/java/com/zalocoders/fashionapp/data/FashionApiService.java
@@ -1,4 +1,10 @@
 package com.zalocoders.fashionapp.data;
 
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface FashionApiService {
+    @GET("fashion_groups")
+    Call<GroupResponse> getGroups();
 }
